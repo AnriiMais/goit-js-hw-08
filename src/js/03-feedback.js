56ec83b212fccb5a2fca36a796c5c5584ffc28b5
Andrii Maistrenko <maystrenko22a@gmail.com>
2021-10-08 17:25:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,17 @@
 import throttle from 'lodash.throttle';
-console.log("document.querySelector('.feedback-form')", document.querySelector('.feedback-form'));
+
 const refs = {
   feedbackForm: document.querySelector('.feedback-form'),
   email: document.querySelector('input[type="email"]'),
   message: document.querySelector('textarea[name="message"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
 };
 refs.feedbackForm.addEventListener('submit', onFormSubmit);
 refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
 if (localStorage.getItem('feedback-form-state')) {
   const saveDefaultForm = JSON.parse(localStorage.getItem('feedback-form-state'));
-  console.log('SaveDefaultForm', saveDefaultForm);
+
   if (saveDefaultForm.email) {
     refs.email.value = saveDefaultForm.email;
   }
@@ -20,12 +22,15 @@ if (localStorage.getItem('feedback-form-state')) {
 function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
   localStorage.removeItem('feedback-form-state');
 }
 function onFormInput(e) {
+  refs.submitBtn.disabled = true;
   const obj = {
     message: refs.feedbackForm.elements.message.value,
     email: refs.feedbackForm.elements.email.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+  if (obj.message && obj.email) refs.submitBtn.disabled = false;
 }
